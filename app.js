d8494c74662a9ee8f3dacfd53ffc42d6dd6cd9a6
V-o-x-a-r-y <isaac.allard@voxary.site>
2024-04-11 10:17:29 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 6576;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let storedLinks = []; // Variable globale pour stocker les liens
+
+app.post('/html', (req, res) => {
+  const html = req.body;
+
+  // Vérifiez si le lien existe déjà dans la liste
+  if (!storedLinks.includes(html)) {
+    // Ajoutez le nouveau lien à la liste des liens stockés
+    storedLinks.push(html);
+    console.log('Liens stockés:', storedLinks);
+    res.send('Liens reçus et stockés avec succès !');
+  } else {
+    console.log('Le lien existe déjà dans la liste.');
+    res.status(400).send('Le lien existe déjà dans la liste.');
+  }
+});
+
+
+// Route pour récupérer les liens stockés
+app.get('/links', (req, res) => {
+  res.json({ links: storedLinks });
+});
+
+app.get('/reset', (req, res) => {
+  storedLinks = []; // Réinitialiser la variable globale storedLinks en affectant un tableau vide
+  res.send('Liens réinitialisés avec succès !');
+});
+
+
+app.listen(port, () => {
+  console.log(`Serveur démarré sur le port ${port}`);
+});
